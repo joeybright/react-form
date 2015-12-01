1e37e572c38733cb91807cde0debf740251b9139
Joey Bright <hello@joeybright.com>
2015-11-30 19:04:00 -0800

    Pass actual data to the Form component

--- components/App.jsx
+++ components/App.jsx
@@ -4,6 +4,8 @@ var React = require('react')
 var ContactForm = require('./Form.jsx')
 // styles
 var styles = require('../styles/app.scss')
+// data
+var contactFormData = require('../data/contact-form.json')
 
 var App = React.createClass({
   handleContactFormError: function(e) {
@@ -17,7 +19,7 @@ var App = React.createClass({
       <div className="w500 ma">
         <h1>Testing a form thing with React</h1>
         <ContactForm
-          data = {""}
+          data = {contactFormData}
           onError = {this.handleContactFormError}
           onSubmit = {this.handleContactFormSubmit} />
       </div>
