2c331ea189cfe56337b8e3eed887920b1c7daf9a
Joey Bright <hello@joeybright.com>
2015-11-30 18:31:31 -0800

    Added email checking

--- components/EmailInput.jsx
+++ components/EmailInput.jsx
@@ -2,31 +2,20 @@
 // components/EmailInput.jsx
 var React = require('react')
 var Input = require('./Input.jsx')
+var errorHandling = require('../helpers/inputErrorHandling.jsx')
 
 var EmailInput = React.createClass({
   getInitialState: function() {
+    // Sets the current error state to blank and false
     return {error: ""}
   },
-  checkError: function(event) {
-    if(this.state.hasError == true) {
-      this.checkPassword(event);
-    }
-  },
-  checkEmail: function(event) {
-    console.log("CHECKING EMAIL");
-    // var email = event.target.value,
-    //     regEx = new Regex(0),
-    //     result = regEx.test(email);
-    // if(email != "") {
-    //   if(result) {
-    //     this.setState({error: ""});
-    //     return this.setState({hasError: false});
-    //   }
-    //   this.setState({error: "error text here"});
-    //   return this.setState({hasError: true});
-    // }
-    // this.setState({error: ""});
-    // return this.setState({hasError: false});
+  // Checks the password against a RegExp
+  checkEmail: function(e) {
+    var regExp = new RegExp("^[a-z0-9](\\.?[a-z0-9_-]){0,}@[a-z0-9-]+\\.([a-z]{1,6}\\.)?[a-z]{2,6}$", "g");
+    // Start off by removing whatever error was already there
+    this.setState({error: ""});
+    // Then run the errorHandling function, passing the event, the RegExp to compare against, and the error message
+    return this.setState(errorHandling(e, regExp, "That doesn't look quite like an email address."));
   },
   render: function () {
     return (
@@ -35,8 +24,9 @@ var EmailInput = React.createClass({
         label={this.props.label}
         placeholder={this.props.placeholder}
         isRequired={this.props.isRequired}
+        errorMessage={this.state.error}
         onBlurFunc={this.checkEmail}
-        onChangeFunc={this.props.onChangeFunc} />
+        onChangeFunc={this.checkEmail} />
     )
   }
 });
